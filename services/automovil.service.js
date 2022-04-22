1664393205afc9d91d314436f9ef239e280d20e1
Felipe Paez <ofpaez87@gmail.com>
2022-04-22 14:55:17 -0500

    Cambio en la declaración del servicio

--- services/automovil.service.js
+++ services/automovil.service.js
@@ -7,7 +7,7 @@ const automovilModelo = automovilesModelo;
 
 
 const buscarTodos = (request, response) => {
-        automovilServicio.find((err, resp) => {
+    automovilModelo.find((err, resp) => {
             response.json(resp); 
         })
 }
@@ -15,7 +15,7 @@ const buscarTodos = (request, response) => {
 const listarModelo = (req, response) => {
     const modelo = req.params.modelo;
     console.log('modelo: ', modelo);
-    automovilServicio.find({modelo: modelo}, (err, resp) => {
+    automovilModelo.find({modelo: modelo}, (err, resp) => {
         console.log(resp);
         response.json(resp);
     });
@@ -31,21 +31,21 @@ const listarColor = (req, response) => {
 
 const listarColorModelo = (request, response) => {
     console.log(request.params.color, request.params.modelo);
-    automovilServicio.find({ color: request.params.color, modelo: request.params.modelo}, (err, res) => {
+    automovilModelo.find({ color: request.params.color, modelo: request.params.modelo}, (err, res) => {
         response.json(res);
     });
 }
 
 const eliminarModelo = (request, response) => {
     const requestModelo    = request.params.modelo;
-    automovilServicio.deleteMany({ color: requestModelo }, (req, res) => {
+    automovilModelo.deleteMany({ color: requestModelo }, (req, res) => {
         response.send(res);
     });
 }
 
 const eliminarColor = (request, response) => {
     const requestColor    = request.params.color;
-    automovilServicio.deleteMany({ color: requestColor }, (req, res) => {
+    automovilModelo.deleteMany({ color: requestColor }, (req, res) => {
         response.send(res);
     });
 }
@@ -53,7 +53,7 @@ const eliminarColor = (request, response) => {
 const eliminarColorModelo = (request, response) => {
     const requestColor   = request.params.color;
     const requestModelo  = request.params.modelo;
-    automovilServicio.deleteMany({ color: requestColor, modelo: requestModelo }, (req, res) => {
+    automovilModelo.deleteMany({ color: requestColor, modelo: requestModelo }, (req, res) => {
         response.send(res);
     });
 }
